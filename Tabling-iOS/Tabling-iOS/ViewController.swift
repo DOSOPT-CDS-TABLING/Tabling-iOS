16c82b16de86e60b42088632e65f17ef3d658f38
ahra1221 <lizze011221@gmail.com>
2023-11-17 14:54:32 +0900

    [⚙️Setting] #1 - 프로젝트 세팅

---
+++ Tabling-iOS/Tabling-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tabling-iOS
+//
+//  Created by 고아라 on 2023/11/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
