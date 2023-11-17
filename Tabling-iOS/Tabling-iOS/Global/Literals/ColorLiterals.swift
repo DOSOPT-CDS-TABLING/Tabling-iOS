b890d316cb3605da07fa3a4df5bb8af501569d29
ahra1221 <lizze011221@gmail.com>
2023-11-17 23:52:00 +0900

    [✨Feat] #1 - Color추가

---
+++ Tabling-iOS/Tabling-iOS/Global/Literals/ColorLiterals.swift
@@ -0,0 +1,55 @@
+//
+//  ColorLiterals.swift
+//  Tabling-iOS
+//
+//  Created by 고아라 on 2023/11/17.
+//
+
+import UIKit
+
+extension UIColor {
+    static var TablingPrimary: UIColor {
+        return UIColor(hex: "#F85A40")
+    }
+    
+    static var TablingWhite: UIColor {
+        return UIColor(hex: "#FFFFFF")
+    }
+    
+    static var Gray000: UIColor {
+        return UIColor(hex: "#EAEAEA")
+    }
+    
+    static var Gray100: UIColor {
+        return UIColor(hex: "#C3C3C6")
+    }
+    
+    static var Gray200: UIColor {
+        return UIColor(hex: "#9EA0A3")
+    }
+    
+    static var Gray300: UIColor {
+        return UIColor(hex: "#62646C")
+    }
+    
+    static var Gray400: UIColor {
+        return UIColor(hex: "#454751")
+    }
+    
+    static var Gray500: UIColor {
+        return UIColor(hex: "#2C2E34")
+    }
+    
+    static var Gray600: UIColor {
+        return UIColor(hex: "#202026")
+    }
+    
+    static var Gray700: UIColor {
+        return UIColor(hex: "#17171B")
+    }
+    
+    static var Gray800: UIColor {
+        return UIColor(hex: "#101012")
+    }
+    
+}
