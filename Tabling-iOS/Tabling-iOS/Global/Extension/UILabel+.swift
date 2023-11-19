f4532e07e1eac7405c31119af77e987f880c815a
MinjaeLee <2alswo7@khu.ac.kr>
2023-11-19 14:45:25 +0900

    [📦Add] #3 - 행간, 자간 설정 Extension 추가

--- Tabling-iOS/Tabling-iOS/Global/Extension/UILabel+.swift
+++ Tabling-iOS/Tabling-iOS/Global/Extension/UILabel+.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 extension UILabel {
+    // 특정 부분만 폰트, 색상 적용
     func partFontChange(targetString: String, font: UIFont, textColor: UIColor) {
         let fullText = self.text ?? ""
         let range = (fullText as NSString).range(of: targetString)
@@ -15,5 +16,21 @@ extension UILabel {
         attributedString.addAttribute(.font, value: font, range: range)
         attributedString.addAttribute(.foregroundColor, value: textColor, range: range)
         self.attributedText = attributedString
+    }
+    
+    // 행간, 자간 적용
+    func setLineAndCharacterSpacing(lineSpacing: CGFloat, characterSpacing: CGFloat) {
+        if let text = self.text {
+            let attributedStr = NSMutableAttributedString(string: text)
+            let style = NSMutableParagraphStyle()
+            style.lineSpacing = lineSpacing
+            attributedStr.addAttribute(NSAttributedString.Key.paragraphStyle,
+                                       value: style,
+                                       range: NSMakeRange(0, attributedStr.length))
+            attributedStr.addAttribute(NSAttributedString.Key.kern,
+                                       value: characterSpacing,
+                                       range: NSMakeRange(0, attributedStr.length))
+            self.attributedText = attributedStr
+        }
     }
 }
