695b1687884e2a5b6f427b67a489d5d0977e6481
ahra1221 <lizze011221@gmail.com>
2023-11-22 01:01:51 +0900

    [✨Feat] #10 - ReserveAlertView 구현

--- Tabling-iOS/Tabling-iOS/Global/Literals/StringLiterals.swift
+++ Tabling-iOS/Tabling-iOS/Global/Literals/StringLiterals.swift
@@ -13,6 +13,16 @@ enum I18N {
         static let nextButtonTitle = "다음으로"
     }
     
+    enum ReserveAlert {
+        static let reserveTitle = "대기 신청 완료"
+        static let waitingTitle = "대기번호"
+        static let waitingNumTitle = "번"
+        static let shopTitle = "매장명"
+        static let personTitle = "인원"
+        static let statusTitle = "신청상태"
+        static let reserveButtonTitle = "대기 내역 확인하기"
+    }
+    
     enum StoreDetail {
         
     }
---
+++ Tabling-iOS/Tabling-iOS/Presentation/ReserveAlert/View/ReserveAlertView.swift
@@ -0,0 +1,293 @@
+//
+//  ReserveAlertView.swift
+//  Tabling-iOS
+//
+//  Created by 고아라 on 2023/11/21.
+//
+
+import UIKit
+
+import SnapKit
+
+protocol ButtonDelegate: AnyObject {
+    func closeButtonTapped()
+    func detailButtonTappd()
+}
+
+final class ReserveAlertView: UIView {
+    
+    // MARK: - Properties
+    
+    weak var buttonDelegate: ButtonDelegate?
+    
+    // MARK: - UI Components
+    
+    private let closeButton: UIButton = {
+        let button = UIButton()
+        button.setImage(ImageLiterals.StoreDetail.ic_exit_circle, for: .normal)
+        return button
+    }()
+    
+    private let reserveTitle: UILabel = {
+        let label = UILabel()
+        label.text = I18N.ReserveAlert.reserveTitle
+        label.textColor = .Gray700
+        label.setLineAndCharacterSpacing(font: .pretendardSemiBold(size: 20))
+        label.partColorChange(targetString: "대기 신청", textColor: .TablingSecondary1)
+        return label
+    }()
+    
+    private let waitingView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .Gray600
+        view.layer.cornerRadius = 4
+        return view
+    }()
+    
+    private let waitingIcon: UIImageView = UIImageView(image: ImageLiterals.StoreDetail.ic_waiting)
+    
+    private let waitingTitle: UILabel = {
+        let label = UILabel()
+        label.text = I18N.ReserveAlert.waitingTitle
+        label.textColor = .TablingSecondary2
+        label.font = .pretendardSemiBold(size: 16)
+        return label
+    }()
+    
+    private let waitingNum: UILabel = {
+        let label = UILabel()
+        label.text = "66"
+        label.textColor = .TablingWhite
+        label.font = .pretendardSemiBold(size: 24)
+        return label
+    }()
+    
+    private let waitingNumTitle: UILabel = {
+        let label = UILabel()
+        label.text = I18N.ReserveAlert.waitingNumTitle
+        label.textColor = .Gray200
+        label.font = .pretendardSemiBold(size: 16)
+        return label
+    }()
+    
+    private let infoIcon: UIImageView = UIImageView(image: ImageLiterals.StoreDetail.ic_info)
+    
+    private let infoTitle: UILabel = {
+        let label = UILabel()
+        label.text = "현재 내 앞 대기 4팀"
+        label.textColor = .TablingPrimary
+        label.font = .pretendardSemiBold(size: 12)
+        return label
+    }()
+    
+    private let detailView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .Gray000
+        view.layer.cornerRadius = 4
+        return view
+    }()
+    
+    private let shopTitle: UILabel = {
+        let label = UILabel()
+        label.text = I18N.ReserveAlert.shopTitle
+        label.textColor = .Gray200
+        label.font = .pretendardSemiBold(size: 14)
+        return label
+    }()
+    
+    private let shopLabel: UILabel = {
+        let label = UILabel()
+        label.text = "파이브가이즈 여의도"
+        label.textColor = .Gray300
+        label.font = .pretendardSemiBold(size: 14)
+        return label
+    }()
+    
+    private let personTitle: UILabel = {
+        let label = UILabel()
+        label.text = I18N.ReserveAlert.personTitle
+        label.textColor = .Gray200
+        label.font = .pretendardSemiBold(size: 14)
+        return label
+    }()
+    
+    private let personLabel: UILabel = {
+        let label = UILabel()
+        label.text = "2명"
+        label.textColor = .Gray300
+        label.font = .pretendardSemiBold(size: 14)
+        return label
+    }()
+    
+    private let statusTitle: UILabel = {
+        let label = UILabel()
+        label.text = I18N.ReserveAlert.statusTitle
+        label.textColor = .Gray200
+        label.font = .pretendardSemiBold(size: 14)
+        return label
+    }()
+    
+    private let statusLabel: UILabel = {
+        let label = UILabel()
+        label.text = "확정예정"
+        label.textColor = .Gray300
+        label.font = .pretendardSemiBold(size: 14)
+        return label
+    }()
+    
+    private let detailButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .TablingPrimary
+        button.setTitle(I18N.ReserveAlert.reserveButtonTitle, for: .normal)
+        button.titleLabel?.font = .pretendardSemiBold(size: 14)
+        button.titleLabel?.textColor = .TablingWhite
+        button.layer.cornerRadius = 8
+        return button
+    }()
+    
+    // MARK: - Life Cycles
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .Gray500
+        
+        setUI()
+        setHierarchy()
+        setLayout()
+        setAddTarget()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension ReserveAlertView {
+    func setUI() {
+        backgroundColor = .TablingWhite
+        layer.cornerRadius = 8
+    }
+    
+    func setHierarchy() {
+        waitingView.addSubviews(waitingIcon, waitingTitle, waitingNumTitle, waitingNum)
+        detailView.addSubviews(shopTitle, shopLabel, personTitle, personLabel, statusTitle, statusLabel)
+        addSubviews(closeButton, reserveTitle, waitingView, infoIcon, infoTitle, detailView, detailButton)
+    }
+    
+    func setLayout() {
+        closeButton.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(19)
+            $0.trailing.equalToSuperview().inset(16)
+            $0.size.equalTo(24)
+        }
+        
+        reserveTitle.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(41)
+            $0.leading.equalToSuperview().inset(16)
+        }
+        
+        waitingView.snp.makeConstraints {
+            $0.top.equalTo(reserveTitle.snp.bottom).offset(16)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(284)
+            $0.height.equalTo(52)
+        }
+        
+        waitingIcon.snp.makeConstraints {
+            $0.centerY.equalToSuperview().inset(18)
+            $0.leading.equalToSuperview().inset(20)
+            $0.size.equalTo(16)
+        }
+        
+        waitingTitle.snp.makeConstraints {
+            $0.centerY.equalToSuperview().inset(14)
+            $0.leading.equalTo(waitingIcon.snp.trailing).offset(8)
+        }
+        
+        waitingNumTitle.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(17)
+            $0.trailing.equalToSuperview().inset(25)
+        }
+        
+        waitingNum.snp.makeConstraints {
+            $0.centerY.equalToSuperview().inset(12)
+            $0.trailing.equalTo(waitingNumTitle.snp.leading).offset(-5)
+        }
+        
+        infoIcon.snp.makeConstraints {
+            $0.top.equalTo(waitingView.snp.bottom).offset(12)
+            $0.leading.equalTo(waitingView.snp.leading)
+            $0.size.equalTo(24)
+        }
+        
+        infoTitle.snp.makeConstraints {
+            $0.top.equalTo(waitingView.snp.bottom).offset(17)
+            $0.leading.equalTo(infoIcon.snp.trailing)
+        }
+        
+        detailView.snp.makeConstraints {
+            $0.top.equalTo(infoIcon.snp.bottom).offset(16)
+            $0.centerX.equalToSuperview().inset(16)
+            $0.width.equalTo(283)
+            $0.height.equalTo(105)
+        }
+        
+        shopTitle.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(12)
+            $0.leading.equalToSuperview().inset(12)
+        }
+        
+        shopLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(12)
+            $0.trailing.equalToSuperview().inset(12)
+        }
+        
+        statusTitle.snp.makeConstraints {
+            $0.bottom.equalToSuperview().inset(12)
+            $0.leading.equalToSuperview().inset(12)
+        }
+        
+        statusLabel.snp.makeConstraints {
+            $0.bottom.equalToSuperview().inset(12)
+            $0.trailing.equalToSuperview().inset(12)
+        }
+        
+        personTitle.snp.makeConstraints {
+            $0.top.equalTo(shopTitle.snp.bottom)
+            $0.bottom.equalTo(statusTitle.snp.top)
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().inset(12)
+        }
+        
+        personLabel.snp.makeConstraints {
+            $0.top.equalTo(shopLabel.snp.bottom)
+            $0.bottom.equalTo(statusLabel.snp.top)
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(12)
+        }
+        
+        detailButton.snp.makeConstraints {
+            $0.bottom.equalToSuperview().inset(33)
+            $0.leading.trailing.equalToSuperview().inset(16)
+            $0.height.equalTo(56)
+        }
+    }
+    
+    func setAddTarget() {
+        closeButton.addTarget(self, action: #selector(isTapped), for: .touchUpInside)
+        detailButton.addTarget(self, action: #selector(isTapped), for: .touchUpInside)
+    }
+    
+    @objc
+    func isTapped(_ sender: UIButton) {
+        switch sender {
+        case closeButton:
+            buttonDelegate?.closeButtonTapped()
+        case detailButton:
+            buttonDelegate?.detailButtonTappd()
+        default:
+            break
+        }
+    }
+}
