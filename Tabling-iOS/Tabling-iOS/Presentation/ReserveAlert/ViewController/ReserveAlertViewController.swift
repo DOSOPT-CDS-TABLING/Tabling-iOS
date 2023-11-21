76df458dc741f2148092689fc4555dd948a7d2c4
ahra1221 <lizze011221@gmail.com>
2023-11-22 02:29:31 +0900

    [✨Feat] ReserveAlertViewController 구현

---
+++ Tabling-iOS/Tabling-iOS/Presentation/ReserveAlert/ViewController/ReserveAlertViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ReserveAlertViewController.swift
+//  Tabling-iOS
+//
+//  Created by 고아라 on 2023/11/21.
+//
+
+import UIKit
+
+import SnapKit
+
+final class ReserveAlertViewController: UIViewController {
+    
+    // MARK: - UI Components
+    
+    private let reserveAlertView = ReserveAlertView()
+    
+    // MARK: - Life Cycles
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUI()
+        setHierachy()
+        setLayout()
+        setDelegate()
+    }
+}
+
+// MARK: - Extensions
+extension ReserveAlertViewController {
+    func setUI() {
+        view.backgroundColor = .black.withAlphaComponent(0.5)
+    }
+    
+    func setHierachy() {
+        view.addSubview(reserveAlertView)
+    }
+    
+    func setDelegate() {
+        reserveAlertView.buttonDelegate = self
+    }
+    
+    func setLayout() {
+        reserveAlertView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.width.equalTo(320)
+            $0.height.equalTo(415)
+        }
+    }
+}
+
+extension ReserveAlertViewController: ButtonDelegate {
+    func closeButtonTapped() {
+        self.dismiss(animated: false)
+    }
+    
+    func detailButtonTappd() {
+        let nav = WaitingDetailViewController()
+        nav.modalPresentationStyle = .fullScreen
+        self.present(nav, animated: false)
+    }
+}
