10188b537a7bbdd8454caa5993af4a83a712cb21
ahra1221 <lizze011221@gmail.com>
2023-11-25 12:24:38 +0900

    [🎨Chore] #14 - 코드리뷰반영

--- Tabling-iOS/Tabling-iOS/Global/Extension/UILabel+.swift
+++ Tabling-iOS/Tabling-iOS/Global/Extension/UILabel+.swift
@@ -19,30 +19,23 @@ extension UILabel {
     }
     
     func partColorChange(targetString: String, textColor: UIColor) {
-            guard let existingText = self.text else {
-                return
-            }
-            
-            let existingAttributes = self.attributedText?.attributes(at: 0, effectiveRange: nil) ?? [:]
-            
-            let attributedStr = NSMutableAttributedString(string: existingText, attributes: existingAttributes)
-            
-            let range = (existingText as NSString).range(of: targetString)
-            attributedStr.addAttribute(NSAttributedString.Key.foregroundColor, value: textColor, range: range)
-            
-            self.attributedText = attributedStr
+        guard let existingText = self.text else {
+            return
+        }
+        let existingAttributes = self.attributedText?.attributes(at: 0, effectiveRange: nil) ?? [:]
+        let attributedStr = NSMutableAttributedString(string: existingText, attributes: existingAttributes)
+        let range = (existingText as NSString).range(of: targetString)
+        attributedStr.addAttribute(NSAttributedString.Key.foregroundColor, value: textColor, range: range)
+        self.attributedText = attributedStr
     }
-
+    
     // 행간, 자간 조절 메소드
     func setLineAndCharacterSpacing(font: UIFont) {
         guard let existingText = self.text else {
             return
         }
-        
         let existingAttributes = self.attributedText?.attributes(at: 0, effectiveRange: nil) ?? [:]
-        
         let attributedStr = NSMutableAttributedString(string: existingText, attributes: existingAttributes)
-        
         let style = NSMutableParagraphStyle()
         style.lineSpacing = 1.5
         attributedStr.addAttribute(NSAttributedString.Key.paragraphStyle,
