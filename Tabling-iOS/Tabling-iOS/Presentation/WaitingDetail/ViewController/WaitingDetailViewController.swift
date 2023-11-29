cb41b5e121f364dc08d262a2b32c48ce45aa77c7
yeonsu <drawingbonny@gmail.com>
2023-11-29 12:04:44 +0900

    [✨Feat] #33 - NavigationBar 구현

--- Tabling-iOS/Tabling-iOS/Presentation/WaitingDetail/ViewController/WaitingDetailViewController.swift
+++ Tabling-iOS/Tabling-iOS/Presentation/WaitingDetail/ViewController/WaitingDetailViewController.swift
@@ -7,23 +7,71 @@
 
 import UIKit
 
-class WaitingDetailViewController: UIViewController {
+final class WaitingDetailViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    // MARK: - Properties
+    
+    // MARK: - UI Components
+    
+    private let myView = View()
+    
+    // MARK: - Life Cycles
+    
+    override func loadView() {
+        super.loadView()
+        
+        view = myView
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUI()
+        setHierarchy()
+        setLayout()
+        setDelegate()
+        setNavigationBar()
     }
-    */
+}
 
+// MARK: - Extensions
+extension WaitingDetailViewController {
+    func setUI() {
+        view.backgroundColor = .white
+    }
+    
+    func setHierarchy() {
+        
+    }
+    
+    func setLayout() {
+        
+    }
+    
+    func setDelegate() {
+        
+    }
+    
+    func setNavigationBar() {
+        let backButton = UIBarButtonItem(image: ImageLiterals.Common.ic_back_b,
+                                         style: .plain,
+                                         target: self,
+                                         action: #selector(backButtonTapped))
+        navigationItem.leftBarButtonItem = backButton
+        navigationItem.leftBarButtonItem?.tintColor = .black
+        
+        navigationItem.title = "대기 상세"
+        
+        let refreshButton = UIBarButtonItem(image: ImageLiterals.Common.ic_refresh,
+                                         style: .plain,
+                                         target: self,
+                                            action: .none)
+        navigationItem.rightBarButtonItem = refreshButton
+        navigationItem.rightBarButtonItem?.tintColor = .black
+    }
+    
+    @objc
+    func backButtonTapped() {
+        self.navigationController?.popViewController(animated: false)
+    }
 }
