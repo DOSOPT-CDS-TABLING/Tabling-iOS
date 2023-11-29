500d04bedcb41f1ae3e98073de3aaced042de644
ahra1221 <lizze011221@gmail.com>
2023-11-29 16:59:45 +0900

    [✨Feat] #36 - 지역인기매장뷰 API 연결

--- Tabling-iOS/Tabling-iOS/Network/Services/StoreListService.swift
+++ Tabling-iOS/Tabling-iOS/Network/Services/StoreListService.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+import Alamofire
+
+final class StoreListService: BaseService {
+    
+    static let shared = StoreListService()
+    
+    private override init() {}
+}
+
+extension StoreListService {
+    func getStoreListAPI(completion: @escaping (NetworkResult<Any>) -> Void) {
+        let url = URLConstant.storeListURL
+        let header: HTTPHeaders = NetworkConstant.header
+        let dataRequest = AF.request(url,
+                                     method: .get,
+                                     encoding: JSONEncoding.default,
+                                     headers: header)
+        
+        dataRequest.responseData { response in
+            switch response.result {
+            case .success:
+                guard let statusCode = response.response?.statusCode else { return }
+                guard let data = response.data else { return }
+                let networkResult = self.judgeStatus(by: statusCode,
+                                                     data,
+                                                     [StoreListEntity].self)
+                completion(networkResult)
+            case .failure:
+                completion(.networkFail)
+            }
+        }
+    }
+}
