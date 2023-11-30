f15d82287b6cdad65ae3fb1b4eba8e9e415ea3a1
ahra1221 <lizze011221@gmail.com>
2023-11-30 15:04:53 +0900

    [✨Feat] #44 - postAPI Service 구현

--- Tabling-iOS/Tabling-iOS/Network/Services/StoreDetailService.swift
+++ Tabling-iOS/Tabling-iOS/Network/Services/StoreDetailService.swift
@@ -24,35 +24,6 @@ extension StoreDetailService {
                                      method: .get,
                                      encoding: JSONEncoding.default,
                                      headers: header)
-
-        dataRequest.responseData { response in
-            switch response.result {
-            case .success:
-                guard let statusCode = response.response?.statusCode else { return }
-                guard let data = response.data else { return }
-                let networkResult = self.judgeStatus(by: statusCode,
-                                                     data,
-                                                     StoreDetailEntity.self)
-                completion(networkResult)
-            case .failure:
-                completion(.networkFail)
-            }
-        }
-    }
-    
-    private func makeTablingParameter(shopID: Int, personCount: Int) -> Parameters {
-        return ["shopID": shopID,
-                "personCount": personCount]
-    }
-    
-    func postTablingStartAPI(shopId: Int, personCount: Int, completion: @escaping (NetworkResult<Any>) -> Void) {
-        let url = URLConstant.waitingStartURL
-        let header: HTTPHeaders = NetworkConstant.header
-        let dataRequest = AF.request(url,
-                                     method: .post,
-                                     parameters: makeTablingParameter(shopID: shopId, personCount: personCount),
-                                     encoding: JSONEncoding.default,
-                                     headers: header)
         
         dataRequest.responseData { response in
             switch response.result {
@@ -68,4 +39,35 @@ extension StoreDetailService {
             }
         }
     }
+    
+    func postTablingStartAPI(
+        shopId: Int,
+        personCount: Int,
+        completion: @escaping (NetworkResult<Any>) -> Void) {
+            let url = URLConstant.waitingStartURL
+            let header: HTTPHeaders = NetworkConstant.header
+            let body = [
+                "shop_id": shopId,
+                "person_count": personCount
+            ]
+            let dataRequest = AF.request(url,
+                                         method: .post,
+                                         parameters: body,
+                                         encoding: JSONEncoding.default,
+                                         headers: header)
+            
+            dataRequest.responseData { response in
+                switch response.result {
+                case .success:
+                    guard let statusCode = response.response?.statusCode else { return }
+                    guard let data = response.data else { return }
+                    let networkResult = self.judgeStatus(by: statusCode,
+                                                         data,
+                                                         ReserveEntity.self)
+                    completion(networkResult)
+                case .failure:
+                    completion(.networkFail)
+                }
+            }
+        }
 }
